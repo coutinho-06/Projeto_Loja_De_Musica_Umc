40bc457b83f87315832a16934598bac19838691b
Gabriella Ferreira Alves <gabriella.alves@sptech.school>
2025-10-12 11:30:01 -0300

    Começo da Página Inicial
    Organização de pastas, criação da navbar, armazenamento de algumas fotos e começo da tela inicial do site.

---
+++ Sistema/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-Vindo a Groove Sound!</title>
+
+    <!-- links de estilo -->
+    <link rel="stylesheet" href="CSS/estiloNavPadrao.css">
+    <link rel="stylesheet" href="CSS/estiloHome.css">
+    <link rel="stylesheet" href="CSS/estiloFooterPadrao.css">
+
+     <!-- link de font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+
+    <nav>
+        <div class="imagemLogo">
+            <img src="IMG/Logos/logo-branca.png">
+        </div>
+        <div class="linksNav">
+            <a href="">Home</a>
+            <a href="">Sobre</a>
+            <a href="">+ Produtos</a>
+            <a href="">Contato</a>
+        </div>
+        <div class="btnNav">
+            <button href="">Cadastrar</button>
+            <button class="btndois" href="">Login</button>
+        </div>
+    </nav>
+
+
+</body>
+</html>
