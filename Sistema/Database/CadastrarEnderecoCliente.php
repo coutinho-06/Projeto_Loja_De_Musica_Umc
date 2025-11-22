ab5aeb59eacbfde9c3f107b4d9399b1f69f66eff
Gabriella Alves <160561279+Gabriella22Ferreira@users.noreply.github.com>
2025-11-21 22:32:10 -0300

    Atualização ou cadastro de endereço feito!!

--- Sistema/Database/CadastrarEnderecoCliente.php
+++ Sistema/Database/CadastrarEnderecoCliente.php
@@ -1,21 +1,41 @@
 <?php
+    
     include "conexao.php";
-    
-    $numR = $_POST['numR'];
-    $cep = $_POST['cep'];
-    $estado = $_POST['estado'];
 
-    
-    $sql = "INSERT INTO endereco(estado,cep,numero) VALUES ('$estado','$cep','$numR')";
-    if (mysqli_query($conn,$sql)) {
-        echo("
-                
-                ");
+    $id_cliente = $_COOKIE["id_cliente"];
+    $index = $_POST["endIndex"]; 
 
-    }else {
-        Header("Location: ../AreaCliente/AcessoCliente.php?errorR=Erro ao inserir o registro!!!". mysqli_error($conn));
+    $numero = $_POST["numR"];
+    $cep = $_POST["cep"];
+    $estado = $_POST["estado"];
+
+    // verifica se existe um endereço na posição escolhida
+    $sql = "SELECT id_endereco FROM endereco WHERE id_cliente = $id_cliente ORDER BY id_endereco ASC LIMIT 1 OFFSET $index";
+    $res = mysqli_query($conn, $sql);
+    $existe = mysqli_fetch_assoc($res);
+
+    // Se existe → ATUALIZA
+    if ($existe) {
+        $id_endereco = $existe["id_endereco"];
+        $sqlUpdate = "
+            UPDATE endereco
+            SET numero = '$numero', cep = '$cep', estado = '$estado'
+            WHERE id_endereco = $id_endereco
+        ";
+
+        mysqli_query($conn, $sqlUpdate);
+    }
+    // Se NÃO existe → CRIA NOVO
+    else {
+        $sqlInsert = "
+            INSERT INTO endereco (id_cliente, numero, cep, estado)
+            VALUES ($id_cliente, '$numero', '$cep', '$estado')
+        ";
+
+        mysqli_query($conn, $sqlInsert);
     }
 
-    
+    header("Location: ../AreaCliente/AcessoCliente.php");
+    exit;
 
 ?>
